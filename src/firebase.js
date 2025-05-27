377f3364914942b0c43a9e69bd587819a551ce5f
AEE EMEBOLF <aeeemebolf@MacBook-Air-de-AEE.local>
2025-05-27 19:42:56 -0300

    Ajustes em gerarPDF, VerPEIs e configuração Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,23 @@
 // src/firebase.js
+
+// Importações do Firebase SDK
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Configuração do seu projeto Firebase
+// Configuração do Firebase do projeto Vivencie PEI
 const firebaseConfig = {
   apiKey: "AIzaSyBcMMoKNIpijamhbADP3aO0Yx7xF0tJg5U",
   authDomain: "vivenciepei.firebaseapp.com",
   projectId: "vivenciepei",
   storageBucket: "vivenciepei.appspot.com",
   messagingSenderId: "591556223675",
-  appId:"1:591556223675:web:804fc4b9b4a56ac4757f9e"
+  appId: "1:591556223675:web:804fc4b9b4a56ac4757f9e"
 };
 
-// Inicializa o Firebase
+// Inicialização do Firebase App
 const app = initializeApp(firebaseConfig);
 
-// Exporta Auth e Firestore
+// Exportação dos serviços utilizados (Auth e Firestore)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
